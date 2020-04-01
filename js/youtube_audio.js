48c054e32a48e5172f47be0958fe59c31f1e69c4
ayangd <jimmyhendrixx909@gmail.com>
2020-04-01 23:56:31 +0700

    Fixed missing image for some videos and stretch images

--- js/youtube_audio.js
+++ js/youtube_audio.js
@@ -38,17 +38,13 @@ chrome.runtime.onMessage.addListener(
             
             let thumbnailImage = document.createElement('img');
             thumbnailImage.className = 'youtube_thumbnail';
-            thumbnailImage.src = 'https://img.youtube.com/vi/' + document.location.href.split('=')[1] + '/maxresdefault.jpg';
-            
-            youtubeThumbnail.addEventListener('locationchange', function() {
-            	thumbnailImage.src = 'https://img.youtube.com/vi/' + document.location.href.split('=')[1].split('&')[0] + '/maxresdefault.jpg';
-            });
+            thumbnailImage.src = 'https://img.youtube.com/vi/' + document.location.href.split('=')[1] + '/0.jpg';
             
             // An observer to detect url change on YouTube
             var observer = new MutationObserver(function(mutations) {
 				mutations.forEach(function(mutationRecord) {
 					if (mutationRecord.target.style.display == 'none') {
-            			thumbnailImage.src = 'https://img.youtube.com/vi/' + document.location.href.split('=')[1].split('&')[0] + '/maxresdefault.jpg';
+            			thumbnailImage.src = 'https://img.youtube.com/vi/' + document.location.href.split('=')[1].split('&')[0] + '/0.jpg';
 					}
 				});
 			});
